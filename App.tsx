0a6667734bf2ab21b23194777ea833d63fca26b0
Milan Bačkonja <milan.backonja@hotmail.com>
2024-11-24 19:44:08 +0100

    Issue

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <Text>Click on any inputs in upper area - you can't scroll</Text>
+          <Text>Click on any inputs in lower area - you can scroll but it's also broken after dismissing keyboard</Text>
+          <StatusBar style="auto" />
+
+          <ScrollView contentContainerStyle={styles.scrollViewContent} automaticallyAdjustKeyboardInsets={true}>
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <View>
+              <Text>Some input</Text>
+              <TextInput style={styles.button} />
+            </View>
+
+            <Button title="Button!" />
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    gap: 10
   },
+  scrollViewContent: {
+    gap: 20,
+    marginHorizontal: 10
+  },
+  button: {
+    borderWidth: 1,
+    height: 50
+  }
 });
